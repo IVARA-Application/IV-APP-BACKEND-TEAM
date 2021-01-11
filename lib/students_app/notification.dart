ed775ba046b09675d9049dec91244471a7c8707c
Sumit Ojha <54531841+sumitsojha88@users.noreply.github.com>
2021-01-12 01:32:44 +0530

    notification

--- lib/students_app/notification.dart
+++ lib/students_app/notification.dart
@@ -1,15 +1,48 @@
 import 'package:flutter/material.dart';
 
-class NotificationPage extends StatelessWidget {
-  static String id = 'NotificationPage';
-  const NotificationPage({Key key}) : super(key: key);
+class NotificationPage extends StatefulWidget{
+  @override
+  _NotificationPageState  createState() => _NotificationPageState();
 
+}
+
+class _NotificationPageState extends State<NotificationPage> {
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
     return Scaffold(
-      body: SafeArea(
-        child: Text("This is notification Page")
+      appBar: AppBar(
+        title: Text("NOTIFICATIONS"),
+        backgroundColor: Colors.lightBlue,
+        actions: <Widget>[
+          Row(
+            children: <Widget>[
+              IconButton(
+                onPressed: () {
+                  debugPrint('TapNotification');
+                },
+                icon: Stack(
+                  children: <Widget>[
+                    Icon(Icons.notifications,
+                        color: Colors.white),
+                    Positioned(
+                      left: 16.0,
+                      child: Icon(Icons.brightness_1,
+                        color: Colors.red,
+                        size: 9.0,
+                      ),
+
+                    )
+                  ],
+                ),
+              )
+
+            ],
+          )
+        ],
       ),
+
+
     );
   }
 }
